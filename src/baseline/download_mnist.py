9a5206ee7121168c0cb5c4c2bee625827236aba2
ZoidBEER <padalkokm@gmail.com>
2023-04-18 01:27:25 +0200

    data ready

---
+++ src/baseline/download_mnist.py
@@ -0,0 +1,8 @@
+import torchvision.datasets
+
+
+if __name__ == "__main__":
+    mnist_trainset = torchvision.datasets.MNIST(root='../../data', train=True, download=True, transform=None)
+    mnist_testset = torchvision.datasets.MNIST(root='../../data', train=False, download=True, transform=None)
+    print(len(mnist_trainset))
+    print(len(mnist_testset))
